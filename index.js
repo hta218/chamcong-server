28de144a8f354878a98606ac676b164f9fd4e682
Huynh Tuan Anh <huynhtuananh21895@gmail.com>
2018-05-15 12:01:03 +0700

    Added PATCH methods to access-control-allow-methods

--- index.js
+++ index.js
@@ -172,7 +172,7 @@ apiRouters.use((req, res, next) => {
       // IE8 does not allow domains to be specified, just the *
       // headers["Access-Control-Allow-Origin"] = req.headers.origin;
       headers["Access-Control-Allow-Origin"] = "*";
-      headers["Access-Control-Allow-Methods"] = "POST, GET, PUT, DELETE, OPTIONS";
+      headers["Access-Control-Allow-Methods"] = "POST, GET, PUT, PATCH, DELETE, OPTIONS";
       headers["Access-Control-Allow-Credentials"] = false;
       headers["Access-Control-Max-Age"] = '86400'; // 24 hours
       headers["Access-Control-Allow-Headers"] = "X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept, x-access-token";
